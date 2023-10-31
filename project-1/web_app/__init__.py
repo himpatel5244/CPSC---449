9639be1f4d944f13d15fbd04cbfd92d0744609bf
himpatel5244 <47892931+himpatel5244@users.noreply.github.com>
2023-10-30 22:14:40 -0700

    Update __init__.py

--- project-1/web_app/__init__.py
+++ project-1/web_app/__init__.py
@@ -8,7 +8,7 @@ DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'jeetpatel'
+    app.config['SECRET_KEY'] = 'himpatel'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
